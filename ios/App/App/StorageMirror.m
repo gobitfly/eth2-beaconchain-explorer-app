cceb211da6deb5755d1d870a87af12ec2f5b2eb7
Manuel macOS <>
2021-05-19 18:00:37 +0200

    ios widget submodule

---
+++ ios/App/App/StorageMirror.m
@@ -0,0 +1,12 @@
+//
+//  StorageMirror.m
+//  App
+//
+//  Created by Kai Pflaume on 28.04.21.
+//
+
+#import <Capacitor/Capacitor.h>
+
+CAP_PLUGIN(StorageMirror, "StorageMirror",
+           CAP_PLUGIN_METHOD(reflect, CAPPluginReturnNone);
+)
