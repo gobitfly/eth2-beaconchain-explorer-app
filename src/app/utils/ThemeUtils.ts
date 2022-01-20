f85de117c45779abca2e6311d257e63d341433a2
Manuel <manuel@caspari.at>
2022-01-20 08:59:51 +0100

    new splash, fixed notification toggles

--- src/app/utils/ThemeUtils.ts
+++ src/app/utils/ThemeUtils.ts
@@ -64,7 +64,7 @@ export default class ThemeUtils {
                 setTimeout(() => {
                     this.colorHandler()
                     splashScreenCallback()
-                }, 1500)
+                }, 1000)
                 return preferenceDarkMode
             }
         )
