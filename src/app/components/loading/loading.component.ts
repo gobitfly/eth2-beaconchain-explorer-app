4d9a9a10e62670777965fe00ce3f25027d9dbab0
Manuel <5877862+manuelsc@users.noreply.github.com>
2024-10-03 09:00:16 +0200

    loading component

---
+++ src/app/components/loading/loading.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+	selector: 'app-loading',
+	standalone: true,
+	imports: [],
+	templateUrl: './loading.component.html',
+	styleUrl: './loading.component.scss',
+})
+export class LoadingComponent {}
